715319e116ef698d8813ce45b92a98bf0d8f925f
sagarregmi2056 <sagarregmi2056@gmail.com>
2025-06-29 16:43:44 +0545

    Configure Babel and Jest for ES modules support

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 /** @type {import('jest').Config} */
 export default {
-  transform: {},
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
   testEnvironment: 'node',
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
-  // Remove extensionsToTreatAsEsm since we're using "type": "module" in package.json
   verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(chalk|commander|inquirer|ora|table)/)'
+  ]
 }; 
